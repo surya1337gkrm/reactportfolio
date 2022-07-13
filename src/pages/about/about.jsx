84fec6a248e6d786c3b19467c32f59d021777833
surya1337gkrm <suryavenkatesh0@gmail.com>
2022-07-13 19:54:44 -0400

    Resume Changes

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -63,7 +63,7 @@ class About extends Component {
                     </div>
                     <div>
                       <a
-                        href='https://drive.google.com/file/d/1UKgxr9dbQkhqrBc8XJFSgwOro468u0R2/view?usp=sharing'
+                        href='https://drive.google.com/file/d/1-tM7df4ToVGmwr3I-umlphc3W-ZlIXdA/view?usp=sharing'
                         target='_blank'
                         rel='noopener noreferrer'>
                         <Button className='m-2' variant='outline-success'>
